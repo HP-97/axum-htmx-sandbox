042b5a8850a690c3089f789b04161de29e1c65d2
Hoa Pham <phamh.2014@gmail.com>
2024-10-27 02:59:44 +1100

    add everything

---
+++ src/main.rs
@@ -0,0 +1,30 @@
+use axum::response::Html;
+use tokio::net::TcpListener;
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
+
+#[tokio::main]
+async fn main() {
+    // Initialise logging
+        tracing_subscriber::registry()
+        .with(
+            tracing_subscriber::EnvFilter::try_from_default_env().unwrap_or_else(|_| {
+                // axum logs rejections from built-in extractors with the `axum::rejection`
+                // target, at `TRACE` level. `axum::rejection=trace` enables showing those events
+                format!(
+                    "{}=debug,tower_http=debug,axum::rejection=trace",
+                    env!("CARGO_CRATE_NAME")
+                )
+                .into()
+            }),
+        )
+        .with(tracing_subscriber::fmt::layer())
+        .init();
+
+    let listener = TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    tracing::debug!("listening on {}", listener.local_addr().unwrap());
+    println!("Hello, world!");
+}
+
+async fn index() -> Html<&'static str> {
+    Html("<h1>heehee</h1>")   
+}
